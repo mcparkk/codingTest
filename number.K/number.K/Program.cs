75dfb337e0cf40cc3ca59d20dd18720e14522bf0
mcparkk <pmc0228@naver.com>
2019-05-23 09:00:12 +0900

    으잌
    으앙

---
+++ number.K/number.K/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number.K
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] result = Solution.solution(1,2,3)
+            
+        }
+    }
+    public class Solution
+    {
+        public int[] solution(int[] array, int[] commands)
+        {
+            int[] answer = new int[] { };
+            
+
+            for(int i =0; i<)
+            
+            return answer;
+        }
+    }
+}
