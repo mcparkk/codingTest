9423125d8c4216f0046fa3eaf5bd8781f1d23979
mcparkk <pmc0228@naver.com>
2019-05-24 18:08:23 +0900

    test
    test

---
+++ divicibleArray/divicibleArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace divicibleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 5, 9, 7, 10 };
+            Console.WriteLine(Solution.solution(arr1,5));
+        }
+    }
+    public class Solution
+    {
+       static public int[] solution(int[] arr, int divisor)
+        {
+            int[] answer = new int[100] ;
+            int len = arr.Length;
+            
+            int j = 0;
+            for (int i =0; i<len;  i++)
+            {
+                if(arr[i]%divisor == 0)
+                {
+                    
+                    answer[j] = arr[i];
+                    j++;
+                }
+            }
+
+           
+            return answer;
+        }
+    }
+}
---
+++ midString/midString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcd";
+            string b = Solution.solution(s);
+            Console.WriteLine(b);
+        }
+    }
+}
+public class Solution
+{
+    static public string solution(string s)
+    {
+        string answer = "";
+        int len = s.Length;
+        char[] arr = s.ToCharArray();
+        int i = len / 2;
+
+        if (len % 2 != 0)
+        {
+            i++;
+            answer = arr[i - 1].ToString();
+        }
+        else if (len % 2 == 0)
+        {
+
+            answer = arr[i - 1].ToString() + arr[i].ToString();
+        }
+        return answer;
+    }
+}
+
+
